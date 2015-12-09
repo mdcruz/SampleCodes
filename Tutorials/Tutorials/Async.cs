9431cc7786e89b900deba11922f5346e4278ff14
mdcruz <marie.descruz@gmail.com>
2015-12-09 10:59:52 +0000

    Initial commit

---
+++ Tutorials/Tutorials/Async.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorials
+{
+    public class Async
+    {
+        static void Main() 
+        {
+        
+        }
+    }
+
+    class AsyncDemo 
+    {
+        public async Task PerformAsyncStuff() 
+        {
+            await Task.Run(() =>
+            {
+                OpenTxtFile();
+            });
+        }
+
+        private async Task OpenTxtFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
